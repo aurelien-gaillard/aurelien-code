b918241801e64d1e0d9457b2c8915c6c73b16d4f
Aurelien Gaillard <aurelien.gaillard@gadz.org>
2021-08-29 16:53:23 +0200

    initial

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './Logo'
+import * as Styled from './styled'
+
+const Navbar = () => {
+  return (
+    <Styled.Wrapper>
+      <Logo />
+    </Styled.Wrapper>
+  )
+}
+
+export default Navbar
